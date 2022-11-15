2b4a2a08a8be04f8b6c7bb51c5c737d6dec5a5e5
1DaNiiL8 <117706811+1DaNiiL8@users.noreply.github.com>
2022-11-15 09:46:34 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада");
+            decimal sum = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Введите срок вклада в месяцах: ");
+            int period = Convert.ToInt32(Console.ReadLine());
+            int i;
+            do 
+            {
+                i += i;
+                sum += sum * 0.07M;
+            }
+            while (i <= 100) ;
+            Console.WriteLine("После  " + period  +  "  месяцев сумма вклвдов составит  " + sum);
+            Console.ReadKey();
+
+        }
+    }
+}
